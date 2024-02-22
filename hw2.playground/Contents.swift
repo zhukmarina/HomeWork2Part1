9965279466256c717daca0d6d38a44bbe920eedd
Marina Zhukova <marinazhukova28@gmail.com>
2024-02-22 20:34:08 +0200

    part 1.4 done

--- hw2.playground/Contents.swift
+++ hw2.playground/Contents.swift
@@ -48,42 +48,6 @@ print("У шлюбі:\(isMarried) ")
 
 print("\nРозділ 1.3 Псевдоніми\n")
 
-/*
-
- Розділ 1.3 - Псевдоніми
-
- 1 "Обізвати" стандартні типи, використовуючи псевдоніми typealias, а саме:
- зарезервувати 3 типи відповідних назв параметрів:
- - тиск
- - вологість
- - температура
-
- 2 оголосити 3 змінні та в якості типу вказати нові зарезервовані псевдоніми із будь-якими значеннями
- 3 використовуючи print() вивсети в консоль з поміткою "Погода вчора" усі параметри
- 4 присвоїти нові значення змінним для тиску, вологості та температури на будь-які інші
- 5 використовуючи print() вивсети в консоль з поміткою "Погода сьогодні" оновлені змінні
-
- 6 перевірте і зробіть Commit (збережіть зміни використовуючт git-клієнт)
-
- * наприклад:
-
-     typealias Humidity = Int // Вологість %
-
-     var humidity: Humidity = 70
-
-     print("Погода вчора:")
-     print("Вологість: \(humidity)%")
-
-     humidity = 65
-
-     print("\nПогода сьогодні:")
-     print("Вологість: \(humidity)%")
-
- * тиск - Pressure 1020 hPa
- * вологість - Humidity 50 %
- * температура - Сelsius 18 С
-
- */
 
 typealias Pressure = Int
 var pressure : Pressure = 735
@@ -112,47 +76,19 @@ print("Температура:\(celsius) C")
 
 print("\nРозділ 1.4 Кортежі\n")
 
-/*
+var profileInfo = (firstName: someUserFirstName, lastName:someUserLastName, age: someUserAge, height: someUserHeight, weight:someUserWeight, isMarried:isMarried)
+var weatherInfo = (humidity, celsius, pressure)
 
- Розділ 1.4 - Кортежі
-
- 1 Використовуючи попередні змінні про користувача (розділ 1.2) та погоду (розділ 1.4),
- оголосити наступні 2 змінні типу кортежі (Tuples):
- - profileInfo (тут заповнити у дужках усі змінні описані раніше для користувача)
- - weatherInfo (тут заповнити у дужках усі змінні описані раніше для погоди)
- 2 у зміній profileInfo, змінити дані про вагу та вік
- 3 використовуючи print() вивести усю інформацію, вказану у змінній profileInfo
- 4 у зміній weatherInfo, змінити дані про температуру
- 5 використовуючи print() вивести усю інформацію, вказану у weatherInfo
-
- 6 перевірте і зробіть Commit (збережіть зміни використовуючт git-клієнт)
-
- * наприклад:
-
-     var profileInfo = (firstName: someUserFirstName, age: someUserAge) // зверніть увагу, що є назва параметру
-     profileInfo.firstName = "another name" // тому через крапку видно не індекс, а назву параметра
-
-     print("Інфорація профіля:")
-     print("Ім'я: \(profileInfo.firstName)")
-
-     var weatherInfo = (humidity, temperature, pressure) // зверніть увагу, що немає назви параметру, лише значення
-     weatherInfo.1 = 19 // тому через крапку видно тільки індекс
-
-     print("\nІнфорація про погоду:")
-     print("Температура: \(weatherInfo.1)")
-
- */
-
-print("Прізвище: ")
-print("Вік: ")
-print("Зріст: см")
-print("Вага: кг")
-print("У шлюбі: ")
+print("Прізвище: \(profileInfo.lastName)")
+print("Вік: \(profileInfo.age)")
+print("Зріст: \(profileInfo.height)см")
+print("Вага:\(profileInfo.weight) кг")
+print("У шлюбі:\(profileInfo.isMarried) ")
 
 print("\nІнфорація про погоду:")
-print("Тиск: hPa")
-print("Вологість: %")
-print("Температура: C")
+print("Тиск:\(weatherInfo.2) hPa")
+print("Вологість: \(weatherInfo.0)%")
+print("Температура: \(weatherInfo.1)C")
 
 
 print("\nРозділ 1.5 Опціонали\n")
